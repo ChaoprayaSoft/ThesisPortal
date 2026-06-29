ad17a2343a1866ffaafeab783f07098e72f4c5cd
Your Name <your-github-email@example.com>
2026-06-29 09:31:06 +0700

    Add Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/components/AuthProvider";
 import ScrollToTop from "@/components/ScrollToTop";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
             © 2026 ChaoprayaSoft, THAILAND
           </footer>
           <ScrollToTop />
+          <Analytics />
         </AuthProvider>
       </body>
     </html>
